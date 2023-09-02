ffdf48abc09582727dd08254be9b53595a3b8ba0
onmikula <onmikula@gmail.com>
2023-09-02 16:56:31 +0200

    Delete func/read.fasta.R

--- func/read.fasta.R
+++
@@ -1,2 +0,0 @@
-read.fasta <- function(file) toupper(ape::read.dna(file, format="fasta", as.matrix=TRUE, as.character=TRUE))
-write.fasta <- function(seq, file) ape::write.dna(toupper(as.matrix(seq)), file, format="fasta")
