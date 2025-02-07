264c44f6964d1858535a3fba8bc64024c3848b71
onmikula <onmikula@gmail.com>
2025-02-07 18:23:25 +0100

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Cytochrome b sequences of Heliophobius
+#'
+#' An alignment of 25 cytochrome b (CYTB) sequences of Silvery Mole-rat (Heliophobius)
+#'   and 3 outgroups (Georychus).
+#'
+#' @format A matrix of mode character with 28 rows (individuals) and 1140 columns (sites).
+#' @source Uhrová et al. 2022 (Mol. Phyl Evol. 167: 107337).
+"cytb"
+
+#' Information about CYTB sequences of Heliophobius
+#'
+#' Information about CYTB sequences of Heliophobius (`heliophobius_cytb` data set)
+#'   including individual ID, locality of origin and its geographical coordinates.
+#'
+#' @format A data frame with 25 rows (individuals) and 6 columns.
+#' @source Uhrová et al. 2022 (Mol. Phyl Evol. 167: 107337).
+"cytbinfo"
+
+#' Color palette 'trubetskoy'
+#'
+#' Twenty distinct colors designed by B. Trubetskoy.
+#'
+#' @format A named vector of mode character specifying colors in HEX format.
+#' @source https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors
+"trubetskoy"
